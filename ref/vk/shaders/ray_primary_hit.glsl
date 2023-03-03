e63176cf0e413dde276f8ccbec2e18705aa8e83a
NightFox <0x4E69676874466F78@users.noreply.github.com>
2023-03-03 03:06:17 +0300

    More correct emissive textures

--- ref/vk/shaders/ray_primary_hit.glsl
+++ ref/vk/shaders/ray_primary_hit.glsl
@@ -56,7 +56,7 @@ void primaryRayHit(rayQueryEXT rq, inout RayPayloadPrimary payload) {
 #if 1
 	// Real correct emissive color
 	//payload.emissive.rgb = kusok.emissive;
-	payload.emissive.rgb = clamp(kusok.emissive / (1.0/3.0) / 25, 0, 1.5) * SRGBtoLINEAR(payload.base_color_a.rgb);
+	payload.emissive.rgb = clamp(((kusok.emissive * (1.0/3.0) / 20) ), 0, 1.0) * SRGBtoLINEAR(payload.base_color_a.rgb);
 #else
 	// Fake texture color
 	if (any(greaterThan(kusok.emissive, vec3(0.))))
