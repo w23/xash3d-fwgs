d16c800843c0454c52d9b5f71f5d08779b9be725
Grigoriy Kabanov <frgrp@mail.ru>
2025-10-31 18:28:02 +0400

    vk: minor

--- ref/vk/vk_decals.h
+++ ref/vk/vk_decals.h
@@ -2,8 +2,6 @@
 
 #include "xash3d_types.h"
 
-struct cl_entity_s;
-
 void VK_ClearDecals( void );
 void VK_DecalShoot( int textureIndex, int entityIndex, int modelIndex, vec3_t pos, int flags, float scale );
 float *VK_DecalSetupVerts( decal_t *pDecal, msurface_t *surf, int texture, int *outCount );
