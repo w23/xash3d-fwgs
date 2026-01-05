de1799beecf3b2b464c355f590aedc90e1141e3f
Grigoriy Kabanov <frgrp@mail.ru>
2026-01-05 16:14:03 +0400

    Update ref/vk/vk_scene.h
    Co-authored-by: Ivan Avdeev <lists@provod.works>

--- ref/vk/vk_scene.h
+++ ref/vk/vk_scene.h
@@ -33,6 +33,6 @@ struct beam_s;
 void CL_DrawBeams( int fTrans, struct beam_s *active_beams );
 void CL_AddCustomBeam( struct cl_entity_s *pEnvBeam );
 
-struct cl_entity_s* VK_GetEntityByIndex(int index);
-model_t* VK_ModelHandle(int index);
-void VK_InitModelsPolys ( void );
+struct cl_entity_s* R_GetEntityByIndex(int index);
+model_t* R_ModelHandle(int index);
+void R_InitModelsPolys ( void );
